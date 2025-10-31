0cb7b7a1fc90d4ef9651a6770112f69d7a79d452
Kareem <k.elsayed@outlook.com>
2025-10-31 18:15:56 +0100

    support for anthropic models added

--- orqest/utils/llm_model.py
+++ orqest/utils/llm_model.py
@@ -3,19 +3,30 @@ import logging
 
 import orqest.config as config
 from pydantic_ai.models.openai import OpenAIModel
+from pydantic_ai.models.anthropic import AnthropicModel
+from pydantic_ai.providers.anthropic import AnthropicProvider
 from pydantic_ai.providers.openai import OpenAIProvider
 
 logger = logging.getLogger(__name__)
 
 def model(**kwargs) -> OpenAIModel:
     """Initialize and return the OpenAI model."""
-    model = OpenAIModel(
-        model_name=config.LLM_MODEL,
-        provider=OpenAIProvider(
-            api_key=config.LLM_API_KEY
-        ),
-        **kwargs,
-    )
+    if 'claude' in config.LLM_MODEL:
+        model = AnthropicModel(
+            model_name=config.LLM_MODEL,
+            provider=AnthropicProvider(
+                api_key=config.LLM_API_KEY
+            ),
+            **kwargs,
+        )
+    else:
+        model = OpenAIModel(
+            model_name=config.LLM_MODEL,
+            provider=OpenAIProvider(
+                api_key=config.LLM_API_KEY
+            ),
+            **kwargs,
+        )
     return model
 
 
